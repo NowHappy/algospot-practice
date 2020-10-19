c238d966f1887f0f127a075c16371027e671648c
happy <happy@nhntoast.com>
2020-10-19 21:44:18 +0900

    BOARDCOVER : 게임판 덮기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,65 +1,85 @@
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Main {
     static int cases;
-    static boolean[][] friends = new boolean[10][10];
-    static int n = 0, m = 0;
+    static int H = 0, W = 0;
+    static int coverType[][][] = {
+        {{0,0},{1,0},{0,1}},
+        {{0,0},{0,1},{1,1}},
+        {{0,0},{1,0},{1,1}},
+        {{0,0},{1,0},{1,-1}}
+    };
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         cases = sc.nextInt();
         for(int c = 1; c <= cases; c++) {
-            n = sc.nextInt();
-            m = sc.nextInt();
-            friends = new boolean[10][10];
-            boolean[] students = new boolean[n];
-            int i = 0, j = 0;
-            while (--m >= 0) {
-                i = sc.nextInt();
-                j = sc.nextInt();
-                friends[i][j] = friends[j][i] = true;
+            H = sc.nextInt();
+            W = sc.nextInt();
+            int[][] matrix = new int[H][W];
+            sc.nextLine();
+            for(int i = 0; i < H; i++) {
+                String line = sc.nextLine();
+                for(int j = 0; j < W; j++) {
+                    if(line.charAt(j) == (char)'#') {
+                        matrix[i][j] = 1;
+                    } else {
+                        matrix[i][j] = 0;
+                    }
+                }
             }
-            System.out.println("---------");
-            System.out.println("case" + c);
 
-            IntStream.range(0, n).forEach(x -> System.out.print(x + ":" + students[x] + " "));
-            System.out.println();
-            System.out.println("---------");
+            //            Arrays.stream(matrix).forEach(ints -> {
+            //                Arrays.stream(ints).forEach(System.out::print);
+            //                System.out.println();
+            //            });
 
-            Arrays.stream(friends).forEach(fr ->{
-                IntStream.range(0,10).forEach(x -> System.out.print(fr[x] + " "));
-                System.out.println();
-            });
-
-            System.out.println("answer : " + mate(students));
+            System.out.println(cover(matrix));
         }
     }
 
+    public static boolean set(int[][] matrix, int y, int x, int type, int delta) {
+        boolean ok = true;
 
-    public static int mate(boolean[] students) {
-
-        int startIndex = -1;
-        for(int i = 0; i < n; i++) {
-            if(!students[i]) {
-                startIndex = i;
-                break;
+        for(int i =0; i < 3; ++i) {
+            int ny = y + coverType[type][i][0];
+            int nx = x + coverType[type][i][1];
+            if(ny < 0 || ny >= H || nx < 0 || nx >= W) {
+                ok = false;
+            } else if ((matrix[ny][nx] += delta) > 1) {
+                ok = false;
             }
         }
 
-        if(startIndex == -1) return 1;
+        return ok;
+    }
 
+    public static int cover(int[][] matrix) {
+        int y = -1;
+        int x = -1;
+        for(int i =0; i < H; ++i) {
+            for(int j = 0; j < W; ++j) {
+                if(matrix[i][j] == 0) {
+                    y = i;
+                    x = j;
+                    break;
+                }
+            }
+            if(y != -1) break;
+        }
+
+        if(y == -1) return 1;
         int ret = 0;
-
-        for(int y = startIndex + 1; y < n; y++){
-            if(!students[y] && friends[y][startIndex]) {
-                students[startIndex] = students[y] = true;
-                ret += mate(students);
-                students[startIndex] = students[y] = false;
+        for(int type = 0; type < 4; ++type) {
+            if(set(matrix, y, x, type, 1)) {
+                ret += cover(matrix);
             }
+            set(matrix, y, x, type, -1);
         }
-
         return ret;
     }
+
+
+
 }
